b4eb057609018eab6e9e10dcac045328be81f8be
Mikcies <huskylpvideos@gmail.com>
2024-04-02 12:52:25 +0200

    Fixed

--- Assets/Scripts/SetBuilding.cs
+++ Assets/Scripts/SetBuilding.cs
@@ -5,6 +5,7 @@ public class SetBuilding : MonoBehaviour
     [field: SerializeField] Building BuiltBuilding;
     public void AssignBuilding()
     {
+        Debug.Log("ahoj");
         UIManager.UIManagerInstance.Hide();
         BuildManager.BuildManagerInstance.SetBuilding(BuiltBuilding);
         GameManager.GameManagerInstance.SetPlayerState(PlayerStates.Building);
